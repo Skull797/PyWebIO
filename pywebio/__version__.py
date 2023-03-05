fc0d81f5c04bf25a2dfd68c9127aeabd9a19bdfb
wang0618 <wang0.618@qq.com>
2023-03-05 13:56:32 +0000

    Build at Sun Mar  5 13:56:32 UTC 2023

--- pywebio/__version__.py
+++ pywebio/__version__.py
@@ -1,8 +1,10 @@
 __package__ = 'pywebio'
 __description__ = 'Write interactive web app in script way.'
 __url__ = 'https://pywebio.readthedocs.io'
-__version__ = "1.7.1"
+__version__ = "1.7.1.dev2303051356"
 __version_info__ = (1, 7, 1, 0)
 __author__ = 'WangWeimin'
 __author_email__ = 'wang0.618@qq.com'
 __license__ = 'MIT'
+
+__commit_hash__ = '95fff37b'
